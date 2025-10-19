a89fdda1f85c222f015359b99ed1e52a3b752bba
Chandra Mouli <chandramouli7616@gmail.com>
2025-10-19 15:12:01 +0530

    Added Azure RG module

---
+++ modules/Azure/resource_group/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the virtual machine."
+  type        = string
+}
+
+variable "location" {
+  description = "Name of the region to deploy the application"
+  type = string
+}
