f1f7aa5262ed80a3d4e8a9fcc026457793cc9b11
Chandra Mouli <chandramouli7616@gmail.com>
2025-10-20 14:37:49 +0530

    updated output folders

---
+++ modules/Azure/nic/output.tf
@@ -0,0 +1,4 @@
+output "nic_id" {
+  value = azurerm_network_interface.nic.id
+  description = "Network Interface resource id"
+}
--- modules/Azure/vm-public-ip/output.tf
+++ modules/Azure/vm-public-ip/output.tf
@@ -2,3 +2,7 @@ output "public_ip_id" {
   value       = azurerm_public_ip.public-ip.id
   description = "Public IP resource id"
 }
+output "public_ip_address" {
+  value       = azurerm_public_ip.public-ip.ip_address
+  description = "Public IP address"
+}
